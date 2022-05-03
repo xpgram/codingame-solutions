ea7df0834f71c2ed1179fc5428117265b0ac3e09
Dei Valko <xp.gram@gmail.com>
2022-05-03 16:19:41 -0700

    format spring-2021 input

--- spring-challenge-2021/spring-challenge-2021.ts
+++ spring-challenge-2021/spring-challenge-2021.ts
@@ -3,82 +3,125 @@
  * Returns a function which returns a Record-type object with all members conforming to type T.  
  * Useful for defining dictionary-like structures without breaking intellisense.
  * ```
- * const dict = confirmType<DataStruct>() ({ Label1: {...}, ...})
+ * const dict = confirmRecord<DataStruct>() ({ Label1: {...}, ...})
  * ``` */
- const confirmType = <T>() => <R extends Record<string, T>>(obj: R) => obj;
-
- /** 
-  * A helper type for parseLine.  
-  * Represents a tuple of types which are the return values for a tuple of `string => any` functions.
-  **/
- type ReturnTypes<T extends Record<number, ((s: string) => any)>> = {
-   -readonly [K in keyof T]: T[K] extends (s: string) => infer R ? R : never;
- }
- 
- /**
-  * Given a tuple of functions, reads input from the input-stream and returns a tuple of corresponding values.
-  * ```
-  * const [ num, bool, str ] = parseLine(Number, Boolean, String);
-  * ``` */
- function parseLine<T extends readonly ((s: string) => any)[]>(...types: T ): ReturnTypes<T> {
-   let inputs: string[] = readline().split(' ');
-   return inputs.map( (s,i) => types[i](s) ) as any;
- }
- 
-
-
-
-
-
-
+const confirmType = <T>() => <R extends Record<string, T>>(obj: R) => obj;
 
 /**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+ * Defines a function which interprets a string as some other type.
+ * Intended for input stream interpretation.
+ */
+type ReadFunc<T> = (s: string) => T;
 
- const numberOfCells: number = parseInt(readline()); // 37
- for (let i = 0; i < numberOfCells; i++) {
-     var inputs: string[] = readline().split(' ');
-     const index: number = parseInt(inputs[0]); // 0 is the center cell, the next cells spiral outwards
-     const richness: number = parseInt(inputs[1]); // 0 if the cell is unusable, 1-3 for usable cells
-     const neigh0: number = parseInt(inputs[2]); // the index of the neighbouring cell for each direction
-     const neigh1: number = parseInt(inputs[3]);
-     const neigh2: number = parseInt(inputs[4]);
-     const neigh3: number = parseInt(inputs[5]);
-     const neigh4: number = parseInt(inputs[6]);
-     const neigh5: number = parseInt(inputs[7]);
- }
+/**
+ * Defines a [key, (key) => value] tuple which is used by readData() to link input stream data
+ * to type-inferred record types.
+ */
+type PropertyTuple = readonly [string, ReadFunc<any>];
+
+/** 
+ * A helper type for parseLine.  
+ * Represents a tuple of types which are the return values for a tuple of `string => any` functions.
+ **/
+type ReturnTypes<T extends Record<number, ReadFunc<any>>> = {
+  -readonly [K in keyof T]: T[K] extends ReadFunc<infer R> ? R : never;
+}
  
- // game loop
- while (true) {
-     const day: number = parseInt(readline()); // the game lasts 24 days: 0-23
-     const nutrients: number = parseInt(readline()); // the base score you gain from the next COMPLETE action
-     var inputs: string[] = readline().split(' ');
-     const sun: number = parseInt(inputs[0]); // your sun points
-     const score: number = parseInt(inputs[1]); // your current score
-     var inputs: string[] = readline().split(' ');
-     const oppSun: number = parseInt(inputs[0]); // opponent's sun points
-     const oppScore: number = parseInt(inputs[1]); // opponent's score
-     const oppIsWaiting: boolean = inputs[2] !== '0'; // whether your opponent is asleep until the next day
-     const numberOfTrees: number = parseInt(readline()); // the current amount of trees
-     for (let i = 0; i < numberOfTrees; i++) {
-         var inputs: string[] = readline().split(' ');
-         const cellIndex: number = parseInt(inputs[0]); // location of this tree
-         const size: number = parseInt(inputs[1]); // size of this tree: 0-3
-         const isMine: boolean = inputs[2] !== '0'; // 1 if this is your tree
-         const isDormant: boolean = inputs[3] !== '0'; // 1 if this tree is dormant
-     }
-     const numberOfPossibleActions: number = parseInt(readline()); // all legal actions
-     for (let i = 0; i < numberOfPossibleActions; i++) {
-         const possibleAction: string = readline(); // try printing something from here to start with
-     }
+/**
+ * Given a tuple of functions, reads input from the input-stream and returns a tuple of corresponding values.
+ * 
+ * Warning: input is split by whitespace. String can only accept a single line token. Use readline() to get all tokens in the line.
+ * ```
+ * const [ num, bool, str ] = parseLine(Number, Boolean, String);
+ * ``` */
+function parseLine<T extends readonly ReadFunc<any>[]>(...types: T ): ReturnTypes<T> {
+  let inputs: string[] = readline().split(' ');
+  return inputs.map( (s,i) => types[i](s) ) as any;
+}
+
+/** A type which infers an object type from a list of PropertyTuples. */
+type ObjectFrom<T extends readonly PropertyTuple[]> = {
+  -readonly [P in T[number] as P[0]]: Extract<P, PropertyTuple>[1] extends ReadFunc<infer R> ? R: never;
+}
+
+/** Given a list of PropertyTuples, reads from the input stream and returns an object literal
+ * composed of the keys and return value types defined by the tuples. */
+function parseStruct<T extends readonly PropertyTuple[]>(properties: T) : ObjectFrom<T>
+{
+  const keys = properties.map( f => f[0] );
+  const funcs = properties.map( f => f[1] );
+  const values = parseLine(...funcs);
+  const record = {};
+  for (let i = 0; i < values.length; i++)
+    record[keys[i]] = values[i];
+  return record as any;
+}
+
+
+module structs {
+
+  export const cellSetup = <const>[
+    ['index', Number],
+    ['richness', Number],
+    ['neigh0', Number],
+    ['neigh1', Number],
+    ['neigh2', Number],
+    ['neigh3', Number],
+    ['neigh4', Number],
+    ['neigh5', Number],
+  ];
+
+  export const cell = <const>[
+    ['index', Number],
+    ['size', Number],
+    ['isMine', Boolean],
+    ['isDormant', Boolean],
+  ];
+
+  export const player = <const>[
+    ['sun', Number],
+    ['score', Number],
+  ];
+
+  export const opponent = <const>[
+    ...player,
+    ['isWaiting', Boolean],
+  ];
+
+}
+
+
+// game setup
+const cells = [] as ObjectFrom<typeof structs.cellSetup>[];
+
+let [ numberOfCells ] = parseLine(Number);
+while (numberOfCells--) {
+  cells.push( parseStruct(structs.cellSetup) );
+}
  
-     // Write an action using console.log()
-     // To debug: console.error('Debug messages...');
- 
- 
-     // GROW cellIdx | SEED sourceIdx targetIdx | COMPLETE cellIdx | WAIT <message>
-     console.log('WAIT');
+// game loop
+while (true) {
+  // Frame setup
+  const day = parseLine(Number);        // the game lasts 24 days: 0-23
+  const nutrients = parseLine(Number);  // the base score you gain from the next COMPLETE action
+  const playerData = parseStruct(structs.player);
+  const opponentData = parseStruct(structs.opponent);
+
+  const celldata = [] as ObjectFrom<typeof structs.cell>[];
+
+  let [ numberOfTrees ] = parseLine(Number);
+  while (numberOfTrees--)
+    celldata.push( parseStruct(structs.cell) );
+
+  const actions = [] as string[];
+
+  let [ possibleActions ] = parseLine(Number);
+  while (possibleActions--)
+    actions.push(readline());
+
+  // Turn Logic
+
+  // Final
+  console.log(actions.shift());
  }
  
