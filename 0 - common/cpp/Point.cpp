29c9ad86c38cee4a3fee06b232a6da905ecf4774
Dei Valko <xp.gram@gmail.com>
2022-05-04 17:05:31 -0700

    update point class

--- 0 - common/cpp/Point.cpp
+++ 0 - common/cpp/Point.cpp
@@ -6,45 +6,57 @@ using namespace std;
 
 class Point {
 public:
-    int x;
-    int y;
+  int x;
+  int y;
 
-    Point() : x(0), y(0) { }
-    Point(const int x, const int y) : x(x), y(y) { }
-    Point(const Point& p) : x(p.x), y(p.y) { }
+  Point() : x(0), y(0) {}
+  Point(const int x, const int y) : x(x), y(y) {}
+  Point(const Point &p) : x(p.x), y(p.y) {}
 
-    operator string() const {
-        stringstream s;
-        s << x << " " << y;
-        return s.str();
-    }
+  operator string() const {
+    stringstream s;
+    s << x << " " << y;
+    return s.str();
+  }
 
-    bool operator== (const Point& other) const {
-        return (x == other.x && y == other.y);
-    }
+  bool operator==(const Point &other) const {
+    return (x == other.x && y == other.y);
+  }
 
-    Point operator+ (const Point& other) const {
-        return Point(x + other.x, y + other.y);
-    }
+  bool operator!=(const Point &other) const {
+    return (x != other.x || y != other.y);
+  }
 
-    Point operator- () const {
-        return Point(-x,-y);
-    }
+  Point operator+(const Point &other) const {
+    return Point(x + other.x, y + other.y);
+  }
 
-    Point operator- (const Point& other) const {
-        return -other + *this;
-    }
+  Point operator-() const {
+    return Point(-x, -y);
+  }
 
-    Point apply(int f(int)) const {
-      return Point(f(x), f(y));
-    }
+  Point operator-(const Point &other) const {
+    return -other + *this;
+  }
 
-    Point abs() const {
-        return Point(::abs(x), ::abs(y));
-    }
+  Point operator*(double n) const {
+    return Point(x*n, y*n);
+  }
 
-    double distanceTo(const Point& other) const {
-        Point vec = (other - *this).abs();
-        return sqrt(vec.x*vec.x + vec.y*vec.y);
-    }
+  Point operator/(double n) const {
+    return Point(x/n, y/n);
+  }
+
+  Point apply(int f(int)) const {
+    return Point(f(x), f(y));
+  }
+
+  Point abs() const {
+    return Point(::abs(x), ::abs(y));
+  }
+
+  double distanceTo(const Point &other) const {
+    Point vec = (other - *this).abs();
+    return sqrt(vec.x * vec.x + vec.y * vec.y);
+  }
 };
